5a8ce51931f9b561f7808f4e4db706a9838d4f69
mennahassan <mennawael7993@gmail.com>
2018-09-03 15:57:22 +0200

    Docker image (#5)
    * bug fixes
    
    * admin can change his/her password
    
    * admin can set timer
    
    * change timer route
    
    * admin can set timer
    
    * score update
    
    * fine touches
    
    * removed console.log
    
    * removed debugger statment
    
    * default timer

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { AdminService } from '../services/admin.service';
 import { QuestionManipulationService } from '../services/question-manipulation.service';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormControl, Validators, FormGroup } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { QuestionComponent } from '../question/question.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,10 +12,19 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  timer: any;
   stats: any = {};
   invitationErr: boolean;
   invitationSuccess: boolean;
-  topPlayers:any[] = [];
+  timerErr: boolean;
+  timerSuccess: boolean;
+  errMessage: string;
+  topPlayers: any[] = [];
+  timerForm = new FormGroup({
+    minutes: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(2), Validators.pattern("[0-9]+")]),
+    seconds: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(2), , Validators.pattern("[0-9]+")])
+
+  });
   constructor(
     private http: Http,
     private adminService: AdminService,
@@ -26,7 +37,6 @@ export class DashboardComponent implements OnInit {
     });
     this.questionManipulation.topPlayers("10").subscribe((res) => {
       this.topPlayers = res.json();
-      console.log(res);
     });
   }
   inviteAdmin(invitationForm: NgForm) {
@@ -36,12 +46,29 @@ export class DashboardComponent implements OnInit {
       this.adminService.iniviteAdmin(email).subscribe((res) => {
         this.invitationSuccess = true;
       }, (err) => {
-        this.invitationErr = true;
+        this.setErrorMessage(err.json()['body']);
       });
     }
   }
   hideAlerts() {
     this.invitationErr = false;
     this.invitationSuccess = false;
+    this.timerSuccess = false;
+    this. timerErr = false;
+  }
+  setErrorMessage(message: string) {
+    this.invitationErr = true;
+    this.errMessage = message;
+  }
+  onTimerChangeFormSubmit() {
+    this.hideAlerts();
+    if (this.timerForm.valid) {
+      let timer = this.timerForm.value.minutes + ":" + this.timerForm.value.seconds;
+      this.adminService.setTimer(timer).subscribe((res) => {
+        this.timerSuccess = true;
+      }, (err) => {
+        this.timerErr = true;
+      })
+    }
   }
 }
